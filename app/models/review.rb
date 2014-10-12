fcd8ec100174db73f9c920dc549a5a3a55b8c254
melonas13 <ReadMe>
2014-10-12 16:37:38 +0100

    Set User Permision

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,7 +5,8 @@ class Review < ActiveRecord::Base
   validates :rating, :comment, presence: true
   validates :rating, numericality: {
     only_integer: true,
-    greater_than_or_equal_to: 5,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5,
     message: "can only be a whole number between 1 and 5"
   }
 end
